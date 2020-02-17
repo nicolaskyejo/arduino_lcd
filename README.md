20645c2834a9498c0334e939b790fc35b3bb2580
nicolaskyejo <36485974+nicolaskyejo@users.noreply.github.com>
2020-02-17 16:57:02 +0200

    some refactoring

--- README.md
+++ README.md
@@ -10,10 +10,10 @@ How to [setup](https://www.arduino.cc/en/Tutorial/LiquidCrystalSerialDisplay)
 $ git clone https://github.com/nicolaskyejo/arduino_lcd.git
 $ cd arduino_lcd
 ```
-* Upload sketch using Arduino IDE, check serial port
+* Upload sketch using Arduino IDE, [check serial port](https://se.mathworks.com/help/supportpkg/arduinoio/ug/find-arduino-port-on-windows-mac-and-linux.html)
 and edit ['weather.py'](https://github.com/nicolaskyejo/arduino_lcd/blob/577705ea709825f2b1517a66c8e5f252e233c762/weather.py#L7)
 accordingly
-* If you don't have pipenv, install it (for example ``python -m pip install pipenv``)
+* If you don't have Pipenv, install it (for example ``python -m pip install pipenv``)
 ```
 $ pipenv install
 $ pipenv shell
--- weather.py
+++ weather.py
@@ -4,42 +4,46 @@ import bs4
 import time
 
 
-port = 'COM3'  # on Windows
-url = 'https://supersaa.fi'  # default setting is 'Helsinki', to change location open url in web browser or send cookies
+PORT = 'COM3'  # on Windows
+URL = 'https://supersaa.fi'  # default setting is 'Helsinki', to change location open url in web browser, change location and send cookies
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
+MINUTES = 60
 
-#TODO
+
+# TODO
 # Replace web scraping with weather api
 
 
-def serial_write(string):
-    with serial.Serial(port) as ser:
-        assert ser.name == port
+def serial_write(text: str) -> None:
+    with serial.Serial(PORT) as ser:
+        assert ser.name == PORT
         time.sleep(2)  # without this, serial write doesn't take place
-        ser.write(string.encode('utf-8'))
+        ser.write(text.encode('utf-8'))
 
 
-def weather_scraper():
-    r = requests.get(url)
-    r.raise_for_status()
+def weather_scraper() -> str:
+    r = requests.get(URL, headers={'User-Agent': 'USER_AGENT'})
+    if r.status_code == requests.codes.ok:
+        soup = bs4.BeautifulSoup(r.text, 'lxml')
+        weather = soup.find(class_='supers-forecast-lane-item-large supers-lane-header-next-hours')
+        weather_top = list(weather.children)[1]
+        weather_bottom = list(weather.children)[3]
 
-    soup = bs4.BeautifulSoup(r.text, 'lxml')
-    weather = soup.find(class_='supers-forecast-lane-item-large supers-lane-header-next-hours')
-    weather_top = list(weather.children)[1]
-    weather_bottom = list(weather.children)[3]
+        temperature = weather_top.find(class_='supers-current-temperature').get_text()
+        wind_speed = weather_top.find(class_='supers-windspeed').get_text()
+        feels_like = weather_bottom.find(class_='supers-temperature').find('span', 'supers-value').get_text()
+        rain_probability = weather_bottom.find(class_='supers-probability-of-rain').find('span', 'supers-value').get_text()
+        rain_amount = weather_bottom.find(class_='supers-rain-amount').find('span', 'supers-value').get_text()
 
-    temperature = weather_top.find(class_='supers-current-temperature').get_text()
-    wind_speed = weather_top.find(class_='supers-windspeed').get_text()
-    feels_like = weather_bottom.find(class_='supers-temperature').find('span', 'supers-value').get_text()
-    rain_probability = weather_bottom.find(class_='supers-probability-of-rain').find('span', 'supers-value').get_text()
-    rain_amount = weather_bottom.find(class_='supers-rain-amount').find('span', 'supers-value').get_text()
-
-    print(temperature, wind_speed, feels_like, rain_probability, rain_amount)  # debug
-    current_time = time.strftime('%H:%M')
-    string = f'Temp is {temperature}C/{feels_like}C at {current_time}'.replace('°', '')
-    return string
+        print(temperature, feels_like, wind_speed, rain_probability, rain_amount, 'at', time.strftime('%A %H:%M'))  # debug print
+        current_time = time.strftime('%H:%M')
+        text = f'Temp is {temperature}C/{feels_like}C at {current_time} -> {rain_probability}'.replace('°', '')
+        return text
+    else:
+        return 'Weather data unavailable'
 
 
 if __name__ == '__main__':
     while True:
         serial_write(weather_scraper())
-        time.sleep(60 * 60)  # better way is to use crontab in Linux or task scheduler in Windows
+        time.sleep(60 * MINUTES)  # better way is to use crontab in Linux or task scheduler in Windows
